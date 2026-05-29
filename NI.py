45f9a1d5d515a951bf342c95949ceb0360e74c82
Carolina Krotsch <carolinakrotsch@Lina-Mac.local>
2026-05-29 15:03:04 +0200

    neu

---
+++ NI.py
@@ -0,0 +1,19 @@
+import nidaqmx
+import nidaqmx.system
+
+system = nidaqmx.system.System.local()
+
+print("Geräte:")
+for dev in system.devices:
+    print(dev)
+
+
+
+
+
+#with nidaqmx.Task() as task:
+ #   task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
+#
+ #   while True:
+  #      value = task.read()
+   #     print(value)
