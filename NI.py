0adc546171753ee21338fb59dc579b52577fb8aa
carolinakrotschi <carolina.krotsch@campus.lmu.de>
2026-05-29 17:03:23 +0200

    neu

--- NI.py
+++ NI.py
@@ -11,9 +11,9 @@ for dev in system.devices:
 
 
 
-#with nidaqmx.Task() as task:
- #   task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
-#
- #   while True:
-  #      value = task.read()
-   #     print(value)
+with nidaqmx.Task() as task:
+    task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
+
+    while True:
+        value = task.read()
+        print(value)
